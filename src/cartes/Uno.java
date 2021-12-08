a3bb788631206064d50a9eb2833b0b2ad517b8d6
ClemWasChoosen <clement.oberhauser@gmail.com>
2021-12-08 15:19:01 +0100

    Ajout Constructeur class Uno

--- src/cartes/Uno.java
+++ src/cartes/Uno.java
@@ -12,11 +12,23 @@ public class Uno {
 
     public Uno() throws ErreurUno {
         Scanner sc = new Scanner(System.in);
-
-        // String input
+        int limiteEntree = 0;
         System.out.println("Entrez le nombre de bots avec qui vous voulez jouer");
-        this.nbJoueur = sc.nextInt();
+        do {
+            this.nbJoueur = sc.nextInt();
+            limiteEntree++;
 
+            if (limiteEntree >= 15)
+                throw new ErreurUno("Impossible de créer le jeu, le nombre de joueur est n'est pas compris entre 1 et 7");
+            if (this.nbJoueur < 0 || this.nbJoueur > 7)
+                System.out.println(this.nbJoueur + " n'est pas compris entre 1 et 7, réessayez");
 
+        }while (this.nbJoueur < 0 || this.nbJoueur > 7);
+
+        initialiser(this.nbJoueur);
+
+    }
+
+    public void initialiser(int nbJoueurL){
     }
 }
