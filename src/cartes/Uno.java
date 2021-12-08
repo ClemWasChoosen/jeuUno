8819f8b981d445e071f570c68ec29d86a116f983
ClemWasChoosen <clement.oberhauser@gmail.com>
2021-12-08 15:08:24 +0100

    Ajout class ErreurUno

--- src/cartes/Uno.java
+++ src/cartes/Uno.java
@@ -1,5 +1,8 @@
 package cartes;
 
+import java.util.Scanner;
+import errorHandler.ErreurUno;
+
 public class Uno {
     private int sens;
     private int joueurActuel;
@@ -7,7 +10,13 @@ public class Uno {
     private boolean etatJeu;
     private Joueur[] tabJoueur;
 
-    public Uno(){
-        
+    public Uno() throws ErreurUno {
+        Scanner sc = new Scanner(System.in);
+
+        // String input
+        System.out.println("Entrez le nombre de bots avec qui vous voulez jouer");
+        this.nbJoueur = sc.nextInt();
+
+
     }
 }
---
+++ src/errorHandler/ErreurUno.java
@@ -0,0 +1,7 @@
+package errorHandler;
+
+public class ErreurUno extends Exception{
+    public ErreurUno(String erreur){
+        super(erreur);
+    }
+}
