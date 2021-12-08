3e10e1f24bd7fa445ca7e7c8100e40445391d4a8
ClemWasChoosen <clement.oberhauser@gmail.com>
2021-12-08 14:41:18 +0100

    Ajout des attributs de Uno

--- src/cartes/Uno.java
+++ src/cartes/Uno.java
@@ -1,6 +1,12 @@
 package cartes;
 
 public class Uno {
+    private int sens;
+    private int joueurActuel;
+    private int nbJoueur;
+    private boolean etatJeu;
+    private Joueur[] tabJoueur;
+
     public Uno(){
         
     }
