55816ab0d8371165cb05d73ae309e79533ff8816
ClemWasChoosen <clement.oberhauser@gmail.com>
2021-12-11 21:29:36 +0100

    PC transfert

--- src/cartes/Uno.java
+++ src/cartes/Uno.java
@@ -19,11 +19,11 @@ public class Uno {
             limiteEntree++;
 
             if (limiteEntree >= 15)
-                throw new ErreurUno("Impossible de créer le jeu, le nombre de joueur est n'est pas compris entre 1 et 7");
-            if (this.nbJoueur < 0 || this.nbJoueur > 7)
+                throw new ErreurUno("Impossible de créer le jeu, le nombre de joueur est n'est pas compris entre 2 et 7");
+            if (this.nbJoueur < 2 || this.nbJoueur > 7)
                 System.out.println(this.nbJoueur + " n'est pas compris entre 1 et 7, réessayez");
 
-        }while (this.nbJoueur < 0 || this.nbJoueur > 7);
+        }while (this.nbJoueur < 2 || this.nbJoueur > 7);
 
         initialiser(this.nbJoueur);
 
@@ -39,21 +39,27 @@ public class Uno {
 
         Scanner sc = new Scanner(System.in);
         String nomJoueur = sc.nextLine();
+        System.out.println("Entrez votre nom de joueur");
         this.tabJoueur[0] = new Humain(nomJoueur);
 
         for (int i = 1; i < nbJoueurL; i++){
             String nomBot;
-            if (i == 1){
+            if (i == 1)
                 nomBot = i + "erBots";
-            }else
+            else
                 nomBot = i + "emeBots";
 
             this.tabJoueur[i] = new Bots(nomBot);
         }
     }
 
-    public void distribuerCarte(){
+    public void distribuerCarte() throws ErreurUno{
+        if (this.tabJoueur == null)
+            throw new ErreurUno("Impossible de distribuer les cartes, le tableau contenant les joueurs est à null");
 
+        for (int i = 0; i < this.nbJoueur; i++){
+            this.tabJoueur[i].setUno(this);
+        }
     }
 
     public void choisirQuiJoue(){
